2856d93fc42a3dcdc903227ebebcc5797e1af7b4
Saad Memon <email2saadmemon>
2018-04-07 16:40:17 +0100

    v1.0

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('PSM_DB_HOST', 'localhost');
+define('PSM_DB_PORT', '3306');
+define('PSM_DB_NAME', 'sysmonitordb');
+define('PSM_DB_USER', 'sysuser');
+define('PSM_DB_PASS', 'test');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'http://localhost/compute_engine/servermon');
+?>
---
+++ install.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHP Server Monitor
+ * Monitor your servers and websites.
+ *
+ * This file is part of PHP Server Monitor.
+ * PHP Server Monitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PHP Server Monitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PHP Server Monitor.  If not, see <http://www.gnu.org/licenses/>.
+
+ **/
+
+define('PSM_INSTALL', true);
+
+require __DIR__ . '/src/bootstrap.php';
+
+$router->run('install');
